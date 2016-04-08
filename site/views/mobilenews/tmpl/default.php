977dce0aee31f65342d111e4ed89a4837440eb3f
Vitaliy <vitaliy.kononenko@freshcode.org>
2016-04-08 17:17:34 +0300

    init

---
+++ site/views/mobilenews/tmpl/default.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+?>
+<!-- <h1><?php echo $this->msg; ?></h1> -->
+<?php
+
+$obj = array(
+  (object) array(
+
+    "userId" => 1,
+    "id" => 1,
+    "title" => "delectus aut autem",
+    "completed" => false
+
+  )
+);
+
+
+echo json_encode($obj);
+jexit();
+?>
