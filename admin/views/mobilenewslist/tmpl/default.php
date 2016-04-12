a398895a03c49f07eb3ff38b74830544e19f7ba1
Vitaliy <vitaliy.kononenko@freshcode.org>
2016-04-12 13:14:56 +0300

    Basic backend

---
+++ admin/views/mobilenewslist/tmpl/default.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted Access');
+?>
+<form action="index.php?option=com_mobilenews&view=mobilenewslist" method="post" id="adminForm" name="adminForm">
+	<table class="table table-striped table-hover">
+		<thead>
+		<tr>
+			<th><?php echo JText::_('COM_MOBILENEWS_NUM'); ?></th>
+      <th><?php echo JText::_('COM_MOBILENEWS_ID'); ?></th>
+      <th width="10%"><?php echo JText::_('COM_MOBILENEWS_MOBILENEWSLIST_TITLE'); ?></th>
+			<th width="70%">
+				<?php echo JText::_('COM_MOBILENEWS_MOBILENEWSLIST_NAME') ;?>
+			</th>
+			<th width="9%">
+				<?php echo JText::_('COM_MOBILENEWS_IMG'); ?>
+			</th>
+			<th width="9%">
+				<?php echo JText::_('COM_MOBILENEWS_DATE'); ?>
+			</th>
+		</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<td colspan="5">
+					<?php echo $this->pagination->getListFooter(); ?>
+				</td>
+			</tr>
+		</tfoot>
+		<tbody>
+			<?php if (!empty($this->items)) : ?>
+				<?php foreach ($this->items as $i => $row) : ?>
+
+					<tr>
+						<td>
+							<?php echo $this->pagination->getRowOffset($i); ?>
+						</td>
+            <td align="center">
+							<?php echo $row->id; ?>
+						</td>
+						<td>
+							<?php echo $row->title; ?>
+						</td>
+            <td>
+							<?php echo $row->text; ?>
+						</td>
+            <td>
+							<img src="<?php echo $row->img; ?>">
+						</td>
+						<td align="center">
+							<?php echo $row->date; ?>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+			<?php endif; ?>
+		</tbody>
+	</table>
+</form>
